4359a3ee4d27812b2338a2545ddb3d5e6a184468
raacker <haven.cpp@gmail.com>
2017-12-21 20:01:30 -0800

    add sudo -u haven

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-sudo apt-get update && sudo apt-get -y upgrade
-mkdir $HOME/setups
-cd $HOME/setups && git clone https://github.com/raacker/setup_vim && cd setup_vim && chmod 700 run.sh && sudo ./run.sh
-cd $HOME/setups && git clone https://github.com/raacker/setup_blog && cd setup_blog && chmod 700 run.sh && sudo ./run.sh
-cd $HOME/setups && git clone https://github.com/raacker/setup_python && cd setup_python && chmod 700 run.sh && sudo ./run.sh
-cd $HOME/setups && git clone https://github.com/raacker/setup_web && cd setup_web && chmod 700 run.sh && sudo ./run.sh
-cd $HOME/setups && git clone https://github.com/raacker/setup_intellij
+apt-get update && apt-get -y upgrade
+sudo -u haven mkdir $HOME/setups
+cd $HOME/setups && sudo -u haven git clone https://github.com/raacker/setup_vim && cd setup_vim && chmod 700 run.sh && sudo ./run.sh
+cd $HOME/setups && sudo -u haven git clone https://github.com/raacker/setup_blog && cd setup_blog && chmod 700 run.sh && sudo ./run.sh
+cd $HOME/setups && sudo -u haven git clone https://github.com/raacker/setup_python && cd setup_python && chmod 700 run.sh && sudo ./run.sh
+cd $HOME/setups && sudo -u haven git clone https://github.com/raacker/setup_web && cd setup_web && chmod 700 run.sh && sudo ./run.sh
+cd $HOME/setups && sudo -u haven git clone https://github.com/raacker/setup_intellij
